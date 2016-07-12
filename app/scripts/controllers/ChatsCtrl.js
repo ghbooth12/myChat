766a5af651a5109c5ec2d4d006b9dceba8302e10
Gahee Heo <ghbooth12@gmail.com>
2016-07-11 22:19:32 -0500

    us4-list-messages-refact: completed

--- app/scripts/controllers/ChatsCtrl.js
+++ app/scripts/controllers/ChatsCtrl.js
@@ -1,25 +1,13 @@
 (function() {
   angular
     .module('myChat')
-    .controller('ChatsCtrl', ['User', 'Room', 'Messages', ChatsCtrl]);
+    .controller('ChatsCtrl', ['Room', 'Messages', ChatsCtrl]);
 
-  function ChatsCtrl(User, Room, Messages) {
+  function ChatsCtrl(Room, Messages) {
     this.messages = function() {
       return Room.activeRoom.messageList;
     };
 
-    this.addMessage = function(newContent) {
-      var msg = {
-        username: User.username,
-        sentAt: new Date().getTime(),
-        roomId: Room.activeRoom.currentRoomId,
-        content: newContent
-      };
-
-      if (newContent !== null) {
-        Messages.add(msg);
-        this.newContent = null;
-      }
-    };
+    this.addMessage = Messages.add;
   }
 })();
